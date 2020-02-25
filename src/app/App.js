0a928bbbcef8a2b63f3c557cbd0e8673a9bdf4fe
Mohammmd Rasel <mohammadrasel1221@gmail.com>
2020-02-26 03:06:30 +0600

    structured project

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h3>Hello Smart village management system.</h3>
+    </div>
+  );
+}
+
+export default App;
